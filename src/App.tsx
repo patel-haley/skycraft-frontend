ccf1994a7356dafd58c77e63ceeda00945de2b5b
patel-haley <118459092+patel-haley@users.noreply.github.com>
2024-11-23 23:28:08 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Header } from './components/Header';
+import { Viewer3D } from './components/Viewer3D';
+import { Controls } from './components/Controls';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-100 relative overflow-hidden">
+      <Header />
+      <main className="w-full h-full pt-16">
+        <Viewer3D />
+        <Controls />
+        <Sidebar />
+      </main>
+    </div>
+  );
+}
+
+export default App;
